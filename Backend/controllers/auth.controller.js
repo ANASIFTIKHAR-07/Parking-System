0369ca08554c3ca2fa642575cd3ca8134e060892
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-07-31 23:37:37 +0500

    Auth Controller

---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,167 @@
+import { Admin } from "../models/admin.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+const addAccessAndRefreshToken = async (userId) => {
+  try {
+    const admin = await Admin.findById(userId);
+    const accessToken = admin.generateAccessToken();
+    const refreshToken = admin.generateRefreshToken();
+    admin.refreshToken = refreshToken;
+    await admin.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating Access Token & Refresh Token"
+    );
+  }
+};
+
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new ApiError(400, "Email and Password are required!!");
+  }
+
+  const admin = await Admin.findOne({
+    email,
+  });
+
+  if (!admin) {
+    throw new ApiError(404, "Admin Does not Exist!!");
+  }
+
+  const isPasswordValid = await admin.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(400, "Password is incorrect!!");
+  }
+  const { accessToken, refreshToken } = await addAccessAndRefreshToken(
+    user._id
+  );
+
+  const loggedInAdmin = await Admin.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  const options = {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          accessToken,
+          refreshToken,
+          loggedInUser,
+        },
+        "User logged In Successfully"
+      )
+    );
+});
+
+const logout = asyncHandler(async (req, res) => {
+  await Admin.findByIdAndUpdate(
+    {
+      $unset: {
+        refreshToken: 1,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged out successfully"));
+});
+
+
+const accessRefreshToken = asyncHandler(async(req, res)=> {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+    if (!incomingRefreshToken) {
+        throw new ApiError(401, "Unauthorized Request ! ")
+    }
+
+    try {
+        const decodedToken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+        )
+
+        const admin = Admin.findById(decodedToken._id)
+
+        if (!admin) {
+            throw new ApiError(401, "Invalid Refresh Token!")
+        }
+
+        if (incomingRefreshToken !== admin?.refreshToken) {
+            throw new ApiError(401, "Refresh Token is expired or used.")
+        }
+
+        const options = {
+            httpOnly: true,
+            secure: true,
+        }
+
+        const {accessToken, refreshToken} = await addAccessAndRefreshToken(user._id)
+
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options )
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    accessToken,
+                    refreshToken: refreshToken,
+                },
+                "Access Token is refreshed successfully"
+            )
+        )
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalide Refresh Token!")
+    }
+})
+
+const getCurrentAdmin = asyncHandler(async (req, res)=> {
+    if(!req.admin){
+        throw new ApiError(404, "Admin not found!");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            req.user,
+            "User Fetched Successfully."
+        )
+    );
+});
+
+export {
+    login,
+    logout,
+    accessRefreshToken,
+    getCurrentAdmin,
+};
