28e5d94d37a71d33219c20c957700e028192e635
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-08-08 11:16:30 +0500

    Routes for admin

--- Backend/routes/admin.routes.js
+++ Backend/routes/admin.routes.js
@@ -5,10 +5,21 @@ import {
     addCompany,
 
 } from "../controllers/admin.controller.js";
-import express, { Router } from "express";
+import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router()
 
-router.route("/get-all-companies").get(verifyJWT, getAllCompanies)
+router.route("/companies").get(verifyJWT, getAllCompanies)
+router.route("/companies").post(verifyJWT, addCompany)
+router.route("/companies/:id").put(verifyJWT, updateCompany)
+router.route("/companies/:id").delete(verifyJWT, deleteCompany)
+
+export default router;
+
+
+// router.route("/all-companies").get(verifyJWT, getAllCompanies)
+// router.route("/company/:id").put(verifyJWT, updateCompany)
+// router.route("/add-company").post(verifyJWT, addCompany)
+// router.route("/company/:id").delete(verifyJWT, deleteCompany)
