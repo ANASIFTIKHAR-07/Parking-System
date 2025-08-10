23df5539e821cc26299799192f7032de14d91127
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-08-10 19:15:27 +0500

    Updation in ParkingSlotSchema

--- Backend/models/parkingSlot.model.js
+++ Backend/models/parkingSlot.model.js
@@ -1,24 +1,24 @@
-import mongoose, {Schema} from "mongoose";
-
+import mongoose, { Schema } from "mongoose";
 
 const parkingSlotSchema = new Schema({
     slotNumber: {
         type: String,
         required: true,
     },
-    isOccupied: {
-        type: Boolean,
-        required: true,
-    },
     floor: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Floor",
+        required: true,
     },
     company: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Company",
+    },
+    employee: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Employee", // assuming you'll have an Employee model
+        default: null,
     }
-})
-
+}, { timestamps: true });
 
 export const ParkingSlot = mongoose.model("ParkingSlot", parkingSlotSchema);
