5ef06f98e361c58270e9fd235458d5175d7abfd5
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-08-11 12:47:19 +0500

    RBAC For Admin

---
+++ Backend/middlewares/role.middleware.js
@@ -0,0 +1,11 @@
+import { ApiError } from "../utils/ApiError.js";
+
+
+export const isAdmin = (req, res, next)=> {
+    console.log("req.user: ", req.user);
+
+    if (req.user?.role !== "admin") {
+        throw new ApiError(403,"Only Admins can access")
+    }
+    next();
+}
