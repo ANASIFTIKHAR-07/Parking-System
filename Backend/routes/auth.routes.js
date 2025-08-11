c8cf58978af9d711e0069b669ec121f4259c4372
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-08-11 12:58:46 +0500

    More RBAC on routes

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,21 +1,18 @@
 import { Router } from "express";
-import { 
-    login,
-    logout,
-    getCurrentAdmin,
-    accessRefreshToken,
+import {
+  login,
+  logout,
+  getCurrentAdmin,
+  accessRefreshToken,
 } from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
+import { isAdmin } from "../middlewares/role.middleware.js";
 
 const router = Router();
 
 router.route("/login").post(login);
-router.route("/logout").post(verifyJWT, logout);
+router.route("/logout").post(verifyJWT, isAdmin, logout);
 router.route("/refresh-token").post(accessRefreshToken);
-router.route("/me").get(verifyJWT, getCurrentAdmin);
-
-
+router.route("/me").get(verifyJWT, isAdmin, getCurrentAdmin);
 
 export default router;
-
