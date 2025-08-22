cfed5ef8532c3d64487e275902d8d57f2126b52d
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-08-23 00:35:59 +0500

    All Auth Api's are working

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -75,7 +75,7 @@ const login = asyncHandler(async (req, res) => {
 
 const logout = asyncHandler(async (req, res) => {
   await Admin.findByIdAndUpdate(
-    req.user._id,
+    req.user._id, 
 
     {
       $unset: {
@@ -155,7 +155,7 @@ const accessRefreshToken = asyncHandler(async (req, res) => {
 });
 
 const getCurrentAdmin = asyncHandler(async (req, res) => {
-  if (!req.admin) {
+  if (!req.user) {
     throw new ApiError(404, "Admin not found!");
   }
 
--- Backend/models/admin.model.js
+++ Backend/models/admin.model.js
@@ -50,7 +50,7 @@ adminSchema.methods.generateAccessToken = function () {
       _id: this._id,
       email: this.email,
       name: this.name,
-      role: this.role,
+      role: this.role, 
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
