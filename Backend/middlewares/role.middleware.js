f883accf6cfa1f3abecfe325c1be573a4123c9c9
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-08-22 23:45:42 +0500

    Role Middleware

--- Backend/middlewares/role.middleware.js
+++ Backend/middlewares/role.middleware.js
@@ -4,7 +4,7 @@ import { ApiError } from "../utils/ApiError.js";
 export const isAdmin = (req, res, next)=> {
     console.log("req.user: ", req.user);
 
-    if (req.user?.role !== "admin") {
+    if (!req.user || req.user?.role !== "admin") {
         throw new ApiError(403,"Only Admins can access")
     }
     next();
