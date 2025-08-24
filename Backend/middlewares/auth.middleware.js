b2440ccb2f2d092e2cdc22854068d7290a7d8819
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-08-24 19:00:07 +0500

    Updation

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -8,7 +8,7 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
-
+      console.log("SECRET:", `"${process.env.ACCESS_TOKEN_SECRET}"`);
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
