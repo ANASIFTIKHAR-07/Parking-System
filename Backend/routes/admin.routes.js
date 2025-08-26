fc8ed6a3ace6e838e13dd2505db78dfc15e9ad8f
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-08-26 21:39:45 +0500

    Company Assignment updation route

--- Backend/routes/admin.routes.js
+++ Backend/routes/admin.routes.js
@@ -25,7 +25,7 @@ router.route("/companies").get(verifyJWT, isAdmin, getAllCompanies);
 router.route("/companies").post(verifyJWT, isAdmin, addCompany);
 router.route("/companies/:id").put(verifyJWT, isAdmin, updateCompany);
 router.route("/companies/:id").delete(verifyJWT, isAdmin, deleteCompany);
-router.route("/companies/floor/:id").put(verifyJWT, isAdmin, assignCompanyToFloor)
+router.route("/companies/assign-floor").put(verifyJWT, isAdmin, assignCompanyToFloor)
 //  Floor Route
 router.route("/floor").post(verifyJWT, isAdmin, createFloor);
 router.route("/floors").get(verifyJWT, isAdmin, getAllFloors);
