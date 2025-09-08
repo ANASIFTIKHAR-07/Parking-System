78fce11d268dfa7379b0ffac7f58a2f5cd641755
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-09-09 00:00:47 +0500

    Frontend setup started

--- Frontend/Parking-System/src/App.jsx
+++ Frontend/Parking-System/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import HomePage from './pages/HomePage'
+
 
 function App() {
 
   return (
     <>
       <div>
+        <HomePage/>
         <h1>Hey this is the Parking system.</h1>
       </div>
     </>
